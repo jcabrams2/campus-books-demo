948f2d9b1cee586986e36ea7ae8b2e7d5613d6d2
jordanCCIT <jordanadposting@gmail.com>
2021-03-25 13:22:02 -0400

    Finished adding a new view!

--- frontend/js/displayHomeView.js
+++ frontend/js/displayHomeView.js
@@ -1,36 +1,39 @@
-const displayHomeView = function (campuses){
-    const mainElement = document.createElement("main");
-    mainElement.classList.add("main-content");
-    const sectionElement = document.createElement("section");
-    sectionElement.classList.add("campus-list");
-    mainElement.appendChild(sectionElement)
+import {displaySingleCampus} from "./displaySingleCampus.js";
 
-    campuses.forEach(campus => {
-        let campusElement = document.createElement("div")
-        campusElement.classList.add("campus");
-        let campusLocationElement = document.createElement("h2");
-        campusLocationElement.classList.add("campus-location")
-        campusLocationElement.innerText = campus.location;
-        let campusTechStackElement = document.createElement("h3");
-        campusTechStackElement.classList.add("campus-tech-stack");
-        campusTechStackElement.innerText = campus.techStack;
-        campusElement.appendChild(campusLocationElement);
-        campusElement.appendChild(campusTechStackElement);
-        sectionElement.appendChild(campusElement);
-        
-    });
-    return mainElement;
-}
+const displayHomeView = function (campuses) {
+  const mainElement = document.createElement("main");
+  mainElement.classList.add("main-content");
+  const sectionElement = document.createElement("section");
+  sectionElement.classList.add("campus-list");
+  mainElement.appendChild(sectionElement);
 
-export{
-    displayHomeView
-}
+  campuses.forEach((campus) => {
+    let campusElement = document.createElement("div");
+    campusElement.classList.add("campus");
+    let campusLocationElement = document.createElement("h2");
+    campusLocationElement.classList.add("campus-location");
+    campusLocationElement.innerText = campus.location;
 
+    campusLocationElement.addEventListener("click", () =>
+      displaySingleCampus(campus)
+    );
+
+    let campusTechStackElement = document.createElement("h3");
+    campusTechStackElement.classList.add("campus-tech-stack");
+    campusTechStackElement.innerText = campus.techStack;
+    campusElement.appendChild(campusLocationElement);
+    campusElement.appendChild(campusTechStackElement);
+    sectionElement.appendChild(campusElement);
+  });
+  return mainElement;
+};
+
+export { displayHomeView };
 
 /* <main class="main-content">
       <section class="campus-list">
         <div class="campus">
-             <h2 class="campus-location">Columbus</h2>
+            <h2 class="campus-location">Columbus</h2>
             <h3 class="campus-tech-stack">Java</h3>
         </div>
         <div class="campus">
