59c7c887441d2ccadd1e6c1161a3a9086188ba7f
David Hooper <david@wecancodeit.org>
2021-03-25 10:50:37 -0600

    site clearly needed more cat photos

--- frontend/js/Footer.js
+++ frontend/js/Footer.js
@@ -1,7 +1,7 @@
 const createFooter = function (){
     const footer = document.createElement("footer");
     footer.classList.add("main-footer");
-    footer.innerHTML='<small class="main-footer__copyright">We Can Code IT &copy;2020</small>'
+    footer.innerHTML='<small class="main-footer__copyright">We Can Code IT &copy;2020 </small><img src="http://placekitten.com/100/50" alt="kitten photos" width="100" hieght="50">'
     return footer;
 }
 
